3a0746d21ceb6c80b783bbdcc68d81dfa9328305
Potatinho23 <mat.aninir@duocuc.cl>
2023-09-08 17:42:05 -0300

    Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
   {
     path: 'asistencia',
     loadChildren: () => import('./pages/asistencia/asistencia.module').then( m => m.AsistenciaPageModule)
+  },  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+
 ];
 
 @NgModule({
---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  usuario={
+    username:"",
+    password:"",
+  }
+
+  constructor(private router:Router,private alertController:AlertController) { }
+
+  ngOnInit() {
+  }
+  onSubmit()
+  {
+    if (this.usuario.username=="wa@coldo.cl" && this.usuario.password=="123"){
+      this.router.navigate(['/home'])
+    }
+    else{
+      
+      this.presentAlert()
+    }
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      subHeader: 'Información',
+      message: "Usuario y/o password incorrectos",
+      buttons: ['OK'],
+      backdropDismiss:false,
+      
+    });
+  }
+}
