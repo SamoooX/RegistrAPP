afc7b5e3da89b7d05e3fb76a645fdd40a5ceddf5
Potatinho23 <mat.aninir@duocuc.cl>
2023-09-10 21:31:19 -0300

    a

--- src/app/perfil/perfil.module.ts
+++ src/app/perfil/perfil.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { PerfilPageRoutingModule } from './perfil-routing.module';
 
 import { PerfilPage } from './perfil.page';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    PerfilPageRoutingModule
+    PerfilPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [PerfilPage]
 })
