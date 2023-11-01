5dcd7cffb0c5f44a3247e2ef97debac2379ba1b8
Potatinho23 <mat.aninir@duocuc.cl>
2023-11-01 14:30:51 -0300

    firebase anashe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,30 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 /*  */
 import { DataService } from './app.component';
 /*  */
+
+// ====== FireBase ======//
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+  ],
   /*  */
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, DataService],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    DataService,
+  ],
   /*  */
   bootstrap: [AppComponent],
 })
