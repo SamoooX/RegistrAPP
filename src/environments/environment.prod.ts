efc9a3c3067eb660829343fa518b80a7163f9d3c
Potatinho23 <mat.aninir@duocuc.cl>
2023-11-01 14:33:09 -0300

    a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyAMiH66L7OGEjJ8ActSL6tcZTdp6vlZpI4",
-    authDomain: "registrapp-ede37.firebaseapp.com",
-    projectId: "registrapp-ede37",
-    storageBucket: "registrapp-ede37.appspot.com",
-    messagingSenderId: "929741834928",
-    appId: "1:929741834928:web:228d98cad7f2480b04c975",
-    measurementId: "G-MK6J0G7S78"
+    apiKey: 'AIzaSyCyBpL7WKjYJKiqxhCp1EMSS-apvO4sa8E',
+    authDomain: 'registrapp-7b995.firebaseapp.com',
+    projectId: 'registrapp-7b995',
+    storageBucket: 'registrapp-7b995.appspot.com',
+    messagingSenderId: '37608024661',
+    appId: '1:37608024661:web:dc450890b34279ca1982dc',
+    measurementId: 'G-PCCRWJQFJ3',
   }
 };
