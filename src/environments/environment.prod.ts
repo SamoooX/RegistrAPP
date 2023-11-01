75efdb99b689b516d412bd38123057621312c5a7
Potatinho23 <mat.aninir@duocuc.cl>
2023-11-01 14:17:02 -0300

    Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAMiH66L7OGEjJ8ActSL6tcZTdp6vlZpI4",
+    authDomain: "registrapp-ede37.firebaseapp.com",
+    projectId: "registrapp-ede37",
+    storageBucket: "registrapp-ede37.appspot.com",
+    messagingSenderId: "929741834928",
+    appId: "1:929741834928:web:228d98cad7f2480b04c975",
+    measurementId: "G-MK6J0G7S78"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyAMiH66L7OGEjJ8ActSL6tcZTdp6vlZpI4",
+    authDomain: "registrapp-ede37.firebaseapp.com",
+    projectId: "registrapp-ede37",
+    storageBucket: "registrapp-ede37.appspot.com",
+    messagingSenderId: "929741834928",
+    appId: "1:929741834928:web:228d98cad7f2480b04c975",
+    measurementId: "G-MK6J0G7S78"
+  }
 };
 
 /*
